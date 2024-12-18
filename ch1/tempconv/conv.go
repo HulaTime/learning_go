ab55bc56f21ccea2d30109ffb0c66f4756fdf1b1
hulatime <hulatime@tutanota.com>
2024-12-18 23:56:46 +0000

    a bunch more jazz

---
+++ ch1/tempconv/conv.go
@@ -0,0 +1,11 @@
+package tempconv
+
+// CToG converts Celsius to Fahrenheit
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+// CToG converts Fahrenheit to Celsius
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
