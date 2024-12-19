ddb4e8bbc6fe36d97303dd4e45ea5a0ec607537e
hulatime <hulatime@tutanota.com>
2024-12-19 00:15:22 +0000

    add Kelvin support to tempconv

--- ch2/2.1/tempconv.go
+++ ch2/2.1/tempconv.go
@@ -5,11 +5,15 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
+	AbsoluteZeroK Kelvin  = 0
+	FreezingK     Kelvin  = 273.15
+	BoilingK      Kelvin  = 373.15
 )
 
 func (c Celsius) String() string {
@@ -20,10 +24,6 @@ func (f Fahrenheit) String() string {
 	return fmt.Sprintf("%g°F", f)
 }
 
-func CToF(c Celsius) Fahrenheit {
-	return Fahrenheit(c*9/5 + 32)
-}
-
-func FToC(f Fahrenheit) Celsius {
-	return Celsius((f - 32) * 5 / 9)
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%g°K", k)
 }
