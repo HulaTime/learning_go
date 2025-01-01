177cd0504ce79b7bfe8250313f68d16689933798
hulatime <hulatime@tutanota.com>
2025-01-01 17:15:57 +0000

    nested structs

--- udemy/go-the-complete-developers-guide/structs/main.go
+++ udemy/go-the-complete-developers-guide/structs/main.go
@@ -2,17 +2,27 @@ package main
 
 import "fmt"
 
+type contact_info struct {
+	email    string
+	postcode string
+}
+
 type person struct {
-	firstName string
-	lastName  string
+	firstName   string
+	lastName    string
+	contactInfo contact_info
 }
 
 func main() {
-	alex := person{"Alex", "Smith"}                      // Go will infer the order based on the order they are defined in the struct declaration
-	bob := person{firstName: "Bob", lastName: "Geldoff"} // Order does not matter
-	var tony person                                      // tony is intialised with no values for firstName or lastName
+	alex := person{"Alex", "Smith", contact_info{"asmith@domain.uk", "SW11 ER6"}}                                                // Go will infer the order based on the order they are defined in the struct declaration
+	bob := person{firstName: "Bob", lastName: "Geldoff", contactInfo: contact_info{email: "bg@domain.uk", postcode: "SW11 ER6"}} // Order does not matter
+	var tony person                                                                                                              // tony is intialised with no values for firstName or lastName
+	var contactInfo contact_info
 	tony.firstName = "Tony"
 	tony.lastName = "Montana"
+	contactInfo.email = "tm@domain.uk"
+	contactInfo.postcode = "SW11 ER6"
+	tony.contactInfo = contactInfo
 
 	fmt.Println(alex, bob, tony)
 }
